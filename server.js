f835d7394eb41aaca4c90a330ea75da4f5e5c126
jonascarvalh <olaffic@gmail.com>
2024-09-09 20:37:10 -0300

    chore: first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(3000, () => {
+    console.log('Server working: http://localhost:3000');
+});
